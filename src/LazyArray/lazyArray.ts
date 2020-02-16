c0965401a2d36e85c914f6299fdbae4b96dde218
tails128 <defendifilippo@gmail.com>
2020-02-16 19:26:28 +0100

    Started to implement the lazyArray class

--- src/LazyArray/lazyArray.ts
+++ src/LazyArray/lazyArray.ts
@@ -1,13 +1,43 @@
-class LazyArray<T> extends Array<T> {
-  sortingFunction: (a: T, b: T) => number;
-  needsSorting: boolean;
+import { LazySortManager } from "../lazySorter/LazySortManager";
 
-  public lazySort(sortingFunction: (a: T, b: T) => number) {
-    //TODO: handle parallelysm
+class LazyArray<T> extends Array<T> {
+  sortingFunction: ((a: T, b: T) => number) | null;
+  needsSorting: boolean = false;
+  sortingKey: keyof T;
+  thread: Promise<any>;
+
+  public lazySort(index: keyof T, sortingFunction: (a: T, b: T) => number) {
+    this.stopLazySorter();
+    this.setLazySorter(index, sortingFunction);
+  }
+
+  private setLazySorter(
+    index: keyof T,
+    sortingFunction: (a: T, b: T) => number
+  ) {
+    this.sortingKey = index;
     this.sortingFunction = sortingFunction;
-    //TODO: in a separate thread
+
+    LazySortManager.addToSorter();
+  }
+
+  private lazySortExecutor(sortingFunction: (a: T, b: T) => number) {
+    //TODO: think about possible issues with parallelysm & solve them
     if (ResourcesEvaluator.isDownTime()) {
       this.sort(sortingFunction);
+      this.needsSorting = false;
+    }
+
+    // Set aside from the previous "if" in order to prevent parallelysm issues.
+    if (!this.needsSorting) {
+      this.stopLazySorter();
+    }
+  }
+
+  public stopLazySorter() {
+    if (this.thread) {
+      this.thread.terminate;
+      this.thread = null;
     }
   }
 }
